ed692a82d685e1d3a97a9398d2f035baabf97b1f
Mikaele Mendonça <mikaelemendonca@gmail.com>
2024-09-18 22:57:48 -0300

    Usando diretivas: v-if v-for v-bind:key ou :key

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteundoPrincipal from './components/ConteundoPrincipal.vue';
 
 export default {
-  components: { Banner }
+  components: { Banner, ConteundoPrincipal }
 }
 </script>
 
 <template>
   <Banner />
+  <ConteundoPrincipal />
 </template>
